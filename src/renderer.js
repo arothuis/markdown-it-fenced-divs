aaae43ee3a238be176c7c5192ee5466efe497682
Alex Rothuis <alex.rothuis@gmail.com>
2023-01-09 17:44:48 +0100

    Add custom default class

--- src/renderer.js
+++ src/renderer.js
@@ -1,6 +1,13 @@
-function renderer() {
+function renderer(options) {
+    const defaultClass = options.defaultClass.trim();
+
     function open(tokens, idx, _options, _env, _slf) {
         tokens[idx].attrJoin("class", tokens[idx].name);
+
+        if (defaultClass.length > 0) {
+            tokens[idx].attrJoin("class", defaultClass);
+        }
+
         return _slf.renderToken(tokens, idx, _options, _env, _slf);
     }
 
